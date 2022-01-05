640dddd1300270daf082f8e963b131cecb333908
Matt Riese <riese.matt@gmail.com>
2022-01-05 10:14:44 -0800

    example of error, can't use + operator on entry that might be null. solution next

--- data-processing/totals.dart
+++ data-processing/totals.dart
@@ -6,5 +6,18 @@ void main(List<String> arguments) {
     exit(1);
   }
   final inputFile = arguments.first;
-  print(inputFile);
+  final lines = File(inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double>{};
+  lines.removeAt(0);
+  for (var line in lines) {
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    final tag = values[5].replaceAll('"', '');
+    if (totalDurationByTag[tag] == null) {
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] += duration;
+    }
+  }
 }
