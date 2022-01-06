845c70fd8f9293e141e7d4cc928b4944079208f3
Matt Riese <riese.matt@gmail.com>
2022-01-05 18:49:49 -0800

    updated where-fcn

--- where-fcn.dart
+++ where-fcn.dart
@@ -1,6 +1,8 @@
 void main() {
   const list = [1, 2, 3, 4];
-  print(where<int>(list, (x) => x % 2 == 0));
+  //print(where<int>(list, (x) => x % 2 == 0));
+  final five = firstWhere(list, (x) => x == 2, orElse: () => -1);
+  print(five);
 }
 
 List<T> where<T>(List<T> items, bool Function(T) f) {
@@ -11,4 +13,14 @@ List<T> where<T>(List<T> items, bool Function(T) f) {
     }
   }
   return result;
+}
+
+T firstWhere<T>(List<T> items, bool Function(T) f,
+    {required Function() orElse}) {
+  for (var item in items) {
+    if (f(item)) {
+      return item;
+    }
+  }
+  return orElse();
 }
